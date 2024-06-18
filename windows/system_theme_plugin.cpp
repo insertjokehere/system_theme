7516e332ed50beb3b2af92bff9e346faca81ec9a
William Hughes <william.hughes@taskaprosthetics.com>
2024-06-19 10:17:48 +1200

    undo

--- windows/system_theme_plugin.cpp
+++ windows/system_theme_plugin.cpp
@@ -10,9 +10,6 @@
 
 #include <mutex>
 
-// https://stackoverflow.com/a/78599923
-#define _DISABLE_CONSTEXPR_MUTEX_CONSTRUCTOR
-
 flutter::EncodableMap getRGBA(windows10colors::RGBA _color)
 {
     /* Converts windows10colors::RGBA to Flutter readable map of following structure.
